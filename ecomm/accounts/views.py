823d99d055b8cea48ffea3e703b460233f22883e
Soham <sohamsarode2312@gmail.com>
2023-06-29 00:19:42 +0530

    Base Created

---
+++ ecomm/accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def login_page(request):
+    return render(request,'accounts/login.html')
+
+def register_page(request):
+    if(request.method == 'POST'):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username=email)
+    if user_obj.exists():
+        messages.warning(request,'User already exists')
+        return HttpResponseRedirect(request.path_info)
+    user_obj= User.objects.create(first_name=first_name,last_name=last_name,email=email,username=email)
+    user_obj.set_password(password)
+    user_obj.save()
+
+    messages.warning(request,'Email has been sent.')
+    
+    return render(request,'accounts/register.html')
--- ecomm/base/models.py
+++ ecomm/base/models.py
@@ -3,8 +3,8 @@ import uuid
 
 class BaseModel(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
