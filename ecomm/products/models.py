bf309dfcdfbd74c448de9ae555b10bcb323f610b
Soham <sohamsarode2312@gmail.com>
2023-07-01 02:58:46 +0530

    Size Variant added completely with price change

--- ecomm/products/models.py
+++ ecomm/products/models.py
@@ -45,6 +45,9 @@ class Product(BaseModel):
 
     def __str__(self):
         return self.product_name
+    
+    def get_product_price_by_size(self,size):
+        return self.price + self.size.get(size_name=size).price 
 
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -8,7 +8,13 @@ from products.models import *
 def get_products(request , slug):
     try:
         product=Product.objects.get(slug=slug)
-        return render(request,'products/products.html',context={'product':product})
+        context={'product':product}
+        if request.GET.get('size'):
+            size = request.GET.get('size')
+            price=product.get_product_price_by_size(size)
+            context['selected_size']=size
+            context['updated_price']=price
+        return render(request,'products/products.html',context=context)
     except Exception as e:
         print(e)
         
